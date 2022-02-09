dafdb994d6ae11ec497fd8b7691e383f545e4ff6
ipluaws <67364414+ipluaws@users.noreply.github.com>
2022-02-09 22:29:47 +0600

    up

--- dev/main.tf
+++ dev/main.tf
@@ -26,3 +26,22 @@ resource "aws_subnet" "public1" {
     Name = "${var.project_name}-net-public1"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.ovalhr-dev.id
+
+  tags = {
+    Name =  "${var.project_name}-IGW"
+  }
+}
+
+resource "aws_route" "route-public" {
+  route_table_id         = aws_vpc.ovalhr-dev.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_vpc.ovalhr-dev.main_route_table_id
+}
